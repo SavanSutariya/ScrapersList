aa286fb58bdd74a56a380d8847e7def136928703
Savan Sutariya <savanpatel5248@gmail.com>
2021-11-15 14:17:29 +0530

    Demo link in readme

--- Readme.md
+++ Readme.md
@@ -1,1 +1,2 @@
 # Scrapers List
+Demo : [Click here..](https://immense-waters-57120.herokuapp.com/)
--- my_app/views.py
+++ my_app/views.py
@@ -1,9 +1,7 @@
 import requests as req
 from django.shortcuts import render,redirect
-from requests import models
 from requests.compat import quote_plus
 from bs4 import BeautifulSoup
-from .models import Search
 # Create your views here
 def home(request):
     return render(request, 'base.html')
@@ -13,7 +11,6 @@ def new_search(request):
         BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
         BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
         query = request.POST.get('query')
-        # Search.objects.create(search=query)
         final_url = BASE_CRAIGSLIST_URL.format(quote_plus(query))
         data = req.get(final_url).text
         soup = BeautifulSoup(data, features='html.parser')
